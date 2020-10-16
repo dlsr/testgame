e197ce51665a888fd3640415e8b2566f36184b92
danillorios <dlsr@cin.ufpe.br>
2020-10-16 16:06:31 -0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-danger')
-            .append("<strong>Um erro foi encontrado no formulário. </strong>");
+            .append("<strong>Um erro foi encontrado no formulário.</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Um erro foi encontrado no formulário."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
