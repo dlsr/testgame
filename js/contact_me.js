99d85987256e3645250d421650002e355858e5f2
danillorios <dlsr@cin.ufpe.br>
2020-10-16 18:08:58 -0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
+      var name = $("input#name").vall();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
