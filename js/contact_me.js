3bbeb0e2514ec95325057feea6c68950feab2d15
danillorios <dlsr@cin.ufpe.br>
2020-10-16 15:52:08 -0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,8 +34,8 @@ $(function() {
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-danger')
+            .append("<strong>Um erro foi encontrado no formulário. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
