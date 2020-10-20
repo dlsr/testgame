dcb040d2493fb6f378226d3fda15389b235cac40
danillorios <dlsr@cin.ufpe.br>
2020-10-20 16:09:16 -0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,7 +46,7 @@ $(function () {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Seu email foi enviado com sucesso! Sextou meu abençoado 😂 </strong>");
+            .append("<strong> Seu email foi enviado com sucesso! </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
